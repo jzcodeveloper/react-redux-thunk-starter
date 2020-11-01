9cca9cd40efc3c69b4cca1a205815df024c90ba3
javier_bislip@hotmail.com <javier_bislip@hotmail.com>
2020-11-01 12:57:29 -0400

    Added react router and created some empty containers

---
+++ src/containers/movie/movie.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container, Heading } from "./styles";
+
+export const Movie = () => {
+  return (
+    <Container>
+      <Heading>Movie</Heading>
+    </Container>
+  );
+};
